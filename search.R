c32081b461a2e640af05e2eff9132c06930d15c8
Benjamin Carter <benjamin.t.carter@gmail.com>
2019-02-12 18:13:13 -0700

    updating

--- search.R
+++ search.R
@@ -8,10 +8,10 @@
 ###############
 
 # check for required packaages are install them if necessary
-list.of.packages <- c("psych","reshape2","car","lme4","ggplot2","tidyverse","data.table")          # list of packages
-new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]        # compare the list to the installed packages list and add missing packages to new list
-if(length(new.packages)) install.packages(new.packages, dependencies = TRUE)                       # install missing packages
-lapply(list.of.packages,library,character.only = TRUE)                                             # load packages
+list.of.packages <- c("psych","reshape2","car","lme4","ggplot2","tidyverse","data.table","dplyr")          # list of packages
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]                # compare the list to the installed packages list and add missing packages to new list
+if(length(new.packages)) install.packages(new.packages, dependencies = TRUE)                               # install missing packages
+lapply(list.of.packages,library,character.only = TRUE)                                                     # load packages
 
 #######################
 # VARIABLES AND PATHS #
@@ -44,37 +44,12 @@ original$Subject <- NULL                                                        
 original$SUBJECT <- gsub("s(\\d+)c\\w","\\1", original$RECORDING_SESSION_LABEL)                         # extract subject numbers and create a new subject column and put them in there.
 original$CONDITION <- gsub("s\\d+c(\\w)","\\1", original$RECORDING_SESSION_LABEL)                       # now do the same thing for caffeine condition
 
-#########
-# MATHS #
-#########
+######################
+# MATHS and WIZARDRY #
+######################
 
 # aggregate the data by subject and session, i.e. compute means and standard deviations for each subject and session
+wreckit <- by(data = list(original$CURRENT_FIX_DURATION,original$NEXT_SAC_AMPLITUDE,original$NEXT_SAC_PEAK_VELOCITY), INDICES = list(original$SUBJECT,original$CONDITION), FUN = describe)
 
-test <- by(data = list(original$CURRENT_FIX_DURATION,original$NEXT_SAC_AMPLITUDE,original$NEXT_SAC_PEAK_VELOCITY), INDICES = list(original$SUBJECT,original$CONDITION), FUN = describe)
-test2 <- data.frame(test, row.names = c())
-print(test2)
-
-# old stuff I tried. everything broke for the saccade amplitude and velocity so I gave up on it.
-# #   do it for fixation duration
-# MeanFix <- aggregate(original$CURRENT_FIX_DURATION, by=list(original$SUBJECT,original$CONDITION), FUN= function(original$CURRENT_FIX_DURATION){fitdistr(original$CURRENT_FIX_DURATION, densfun = "normal")$estimate} )
-# names(MeanFix) <- c("Subject","Condition","fixMean")
-# SDFix <- aggregate(original$CURRENT_FIX_DURATION, by=list(original$SUBJECT,original$CONDITION), FUN=sd)
-# names(SDFix) <- c("Subject","Condition","fixSD")
-# 
-# #   do it for saccade amplitude
-# MeanSacAmp <- aggregate(original$NEXT_SAC_AMPLITUDE, by=list(original$SUBJECT,original$CONDITION), FUN = mean)
-# names(MeanSacAmp) <- c("Subject","Condition","sacAmpMean")
-# SDSacAmp <- aggregate(original$NEXT_SAC_AMPLITUDE, by=list(original$SUBJECT,original$CONDITION), FUN=sd)
-# names(SDSacAmp) <- c("Subject","Condition","sacAmpSD")
-# 
-# #   do it for saccade velocity
-# MeanSacVel <- aggregate(original$NEXT_SAC_AVG_VELOCITY, by=list(original$SUBJECT,original$CONDITION), FUN=mean)
-# names(MeanSacVel) <- c("Subject","Condition","sacVelMean")
-# SDSacVel <- aggregate(original$NEXT_SAC_AVG_VELOCITY, by=list(original$SUBJECT,original$CONDITION), FUN=sd)
-# names(SDSacVel) <- c("Subject","Condition","sacVelSD")
-
-
-# #   bring it all together into one data frame
-# aggAll <- merge(MeanFix,SDFix,MeanSacAmp,SDSacAmp,MeanSacVel,SDSacVel,by=c("Subject","Condition"))
-# 
-# test <- by(original$NEXT_SAC_AVG_VELOCITY, list(original$SUBJECT,original$CONDITION), FUN = stat.desc)
+# convert the list of matrices (aka wreckit) to a single matrix
+for (i in )
